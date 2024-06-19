da6b6456e7778a4fd9f1a891d4442363ef05ab4c
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-06-20 01:29:48 +0800

    deps: remove `@tailwindcss/typography`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,5 @@
 import type { Config } from 'tailwindcss';
-import typo from '@tailwindcss/typography';
-
 export default {
     experimental: { optimizeUniversalDefaults: true },
     content: ['./src/**/*.{css,html,js,svelte,ts}'],
-    plugins: [typo],
 } satisfies Config;
