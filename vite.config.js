42e117d9aae4da598fce72e2d59bcdcb4e4e3387
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-07-08 07:39:10 +0800

    chore: revert PostCSS configuration externalization

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,11 @@
 import { defineConfig } from 'vite';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
-export default defineConfig({ plugins: [sveltekit(), purgeCss()] });
+
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+
+export default defineConfig({
+    plugins: [sveltekit(), purgeCss()],
+    css: { postcss: { plugins: [tailwindcss, autoprefixer] } },
+});
