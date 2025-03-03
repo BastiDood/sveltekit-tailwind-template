4f0b6fbd5fe1fb292a905d85445ecac6ab6c934b
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2025-03-04 03:10:34 +0800

    chore: remove unused Tailwind configuration file

--- tailwind.config.ts
+++
@@ -1,5 +0,0 @@
-import type { Config } from 'tailwindcss';
-export default {
-    experimental: { optimizeUniversalDefaults: true },
-    content: ['./src/**/*.{html,js,svelte,ts}'],
-} satisfies Config;
